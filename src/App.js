4cb99b7eb9d962a1c0c838c396214ee434a5c960
lucaspires-source <pires.lucas94@gmail.com>
2021-06-05 14:48:29 -0300

    dragging implemented

--- src/App.js
+++ src/App.js
@@ -2,13 +2,17 @@ import React from "react";
 import Header from "./components/Header";
 import GlobalStyle from "./styles/global";
 import Board from "./components/Board";
+
+import { DndProvider } from 'react-dnd';
+import {HTML5Backend } from 'react-dnd-html5-backend';
+
 const App = () => {
   return (
-    <>
+    <DndProvider backend={HTML5Backend}>
       <Header />
       <Board />
       <GlobalStyle />
-    </>
+    </DndProvider>
   );
 };
 
--- src/components/List/styles.js
+++ src/components/List/styles.js
@@ -1,36 +1,36 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-padding: 0 15px;
-height:100%;
-flex: 0 0 320px;
-opacity:${props => props.done ? 0.6 : 1};
-&+div{
-    border-left:  1px solid rgba(0,0,0,0.5);
-}
+  padding: 0 15px;
+  height: 100%;
+  flex: 0 0 320px;
+  opacity: ${(props) => (props.done ? 0.6 : 1)};
+  & + div {
+    border-left: 1px solid rgba(0, 0, 0, 0.5);
+  }
 
-header{
+  header {
     display: flex;
-    justify-content:space-between;
+    justify-content: space-between;
     align-items: center;
-    height:42px
-}
-h2{
+    height: 42px;
+  }
+  h2 {
     font-weight: 500;
-    font-size:16px;
+    font-size: 16px;
     padding: 0 10px;
-}
+  }
 
-button{
-    width:42px;
-    height:42px;
+  button {
+    width: 42px;
+    height: 42px;
     border-radius: 18px;
     background: #3b5bfd;
     border: 0;
     cursor: pointer;
-}
+  }
 
-ul{
+  ul {
     margin-top: 30px;
-}
+  }
 `;
