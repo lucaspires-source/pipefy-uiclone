797d3ba27fd7e1726551c743ad214e77d280c5b8
lucaspires-source <pires.lucas94@gmail.com>
2021-06-05 11:45:00 -0300

    code structure

--- src/components/Header/styles.js
+++ src/components/Header/styles.js
@@ -4,7 +4,7 @@ import styled from "styled-components";
 export const Container = styled.div `
 height:80px;
 padding: 0 30px;
-background:#33C2FF;
+background:#26ACE5;
 color:#fff;
 display:flex;
 align-items:center;
